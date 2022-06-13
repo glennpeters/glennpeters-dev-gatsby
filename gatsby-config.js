7c8ebeb8e11ec9a546063b159db6c8e81ee870e5
Glenn Peters <glenn.b.peters@gmail.com>
2022-06-12 23:18:36 -0700

    Update siteMetadata in gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,10 @@
 module.exports = {
   siteMetadata: {
-    title: `glennpeters-dev`,
+    title: `Glenn Peters`,
+    description: `Resume and portfolio website`,
+    siteUrl: `https://www.glennpeters.dev`,
+    author: `@gentlerpens`,
+        // title: `glennpeters-dev`,
     siteUrl: `https://www.glennpeters.dev`
   },
   plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-less", {
